c5336550f51275f15433becf3b13d4e89d959506
Ludovic Fabrèges <ludovic.fabreges@gmail.com>
2022-04-03 18:18:00 +0200

    Add two new levels

---
+++ levels/009.lua
@@ -0,0 +1,50 @@
+return {
+  ball = {
+    x = 100,
+    y = 460,
+  },
+  obstacles = {
+    {
+      type = "horizontal-barrier",
+      x = 240,
+      y = 90,
+      width = 60,
+      height = 30,
+    },
+    {
+      type = "vertical-barrier",
+      x = 180,
+      y = 0,
+      width = 30,
+      height = 60,
+    },
+  },
+  stars = {
+    one = 3,
+    two = 2,
+    three = 1,
+  },
+  targets = {
+    {
+      type = "normal",
+      x = 220,
+      y = 300,
+      width = 40,
+      height = 40,
+    },
+    {
+      type = "normal",
+      x = 80,
+      y = 200,
+      width = 40,
+      height = 40,
+    },
+    {
+      type = "hard",
+      x = 240,
+      y = 20,
+      width = 40,
+      height = 40,
+    },
+  },
+}
