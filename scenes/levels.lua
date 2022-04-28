0c030d7485f50c0e24d2e3275c904aff513abb56
Ludovic Fabrèges <ludovic.fabreges@gmail.com>
2022-04-28 19:55:14 +0200

    Simple filler to create equal space between levels

--- scenes/levels.lua
+++ scenes/levels.lua
@@ -66,8 +66,9 @@ function scene:createContentView()
     if worldScores[level.name] then
       components.newScore(levelStack, 20, worldScores[level.name].numberOfStars)
     else
-      local noScore = components.newScore(levelStack, 20, 0)
-      noScore.alpha = 0.2
+      local filler = display.newRect(0, 0, 1, 20)
+      filler.isVisible = false
+      levelStack:insert(filler)
     end
   end
 
